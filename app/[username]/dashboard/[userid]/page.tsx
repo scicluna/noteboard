e40968ed7a520425e04e775dea09f5458404af70
scicluna <88948700+scicluna@users.noreply.github.com>
2023-07-30 14:01:40 -0400

    some more restructuring and prep

---
+++ app/[username]/dashboard/[userid]/page.tsx
@@ -0,0 +1,5 @@
+export default function UserDash() {
+    return (
+        <h1>User Dash</h1>
+    )
+}
---
+++ app/[username]/layout.tsx
@@ -0,0 +1,11 @@
+export default function UserLayout({
+    children, // will be a page or nested layout
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <section>
+            {children}
+        </section>
+    )
+}
